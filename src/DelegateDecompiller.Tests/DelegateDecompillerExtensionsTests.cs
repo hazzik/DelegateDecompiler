89be241059ca32871e60262f6e8d8560c8209ff8
Alexander I. Zaytsev <hazzik@gmail.com>
2012-10-19 16:19:30 +0600

    Add simplest test (without decompilation yet)

---
+++ src/DelegateDecompiller.Tests/DelegateDecompillerExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace DelegateDecompiller.Tests
+{
+    public class DelegateDecompillerExtensionsTests
+    {
+        [Fact]
+        public void ShouldBeAbleToDecompileExpression()
+        {
+            Expression<Func<object, object>> expression = o => o;
+
+            var compiled = expression.Compile();
+
+            var decompilled = compiled.Decompile();
+
+            Assert.Equal(expression.ToString(), decompilled.ToString());
+        }
+    }
+}
---
+++ src/DelegateDecompiller/DelegateDecompillerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace DelegateDecompiller
+{
+    public static class DelegateDecompillerExtensions
+    {
+        public static LambdaExpression Decompile<TDelegate>(this TDelegate @delegate)
+        {
+            var parameter = Expression.Parameter(typeof (object), "o");
+            return Expression.Lambda(parameter, parameter);
+        }
+    }
+}
