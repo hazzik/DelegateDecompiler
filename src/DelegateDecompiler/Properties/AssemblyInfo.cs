fd6e35712c5b1d22a054713128bee5b494460e95
Alexander I. Zaytsev <hazzik@gmail.com>
2012-10-23 20:04:49 +0600

    Fix typo in Assembly title (also this reflects nuget package title)
    - Set version 0.4.1

--- src/DelegateDecompiler/Properties/AssemblyInfo.cs
+++ src/DelegateDecompiler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DelegateDecompiller")]
+[assembly: AssemblyTitle("DelegateDecompiler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DelegateDecompiller")]
+[assembly: AssemblyProduct("DelegateDecompiler")]
 [assembly: AssemblyCopyright("Copyright © hazzik 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
