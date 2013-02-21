2259b76f89a9f507bee1c78052aae293a33fe57b
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-22 10:26:53 +1300

    Compare DebugView of expressions in test.

--- src/DelegateDecompiler.Tests/DecompilerTestsBase.cs
+++ src/DelegateDecompiler.Tests/DecompilerTestsBase.cs
@@ -6,12 +6,21 @@ namespace DelegateDecompiler.Tests
 {
     public class DecompilerTestsBase
     {
+        private static readonly Func<Expression, string> debugView = BuildDebugView();
+
+        private static Func<Expression, string> BuildDebugView()
+        {
+            var parameter = Expression.Parameter(typeof (Expression), "e");
+            return Expression.Lambda<Func<Expression, string>>(Expression.Property(parameter, "DebugView"), parameter).Compile();
+        }
+
         protected static void Test<T>(Expression<T> expected, T compiled)
         {
-            //There is no way to direct cast T to Delegate, so we are using dynamic trick
-            var decompiled = Delegate.Combine(new Delegate[] { (dynamic) compiled }).Decompile();
+            //Double cast required as we can not convert T to Delegate directly
+            var decompiled = ((Delegate) ((object) compiled)).Decompile();
 
             Assert.Equal(expected.ToString(), decompiled.ToString());
+            Assert.Equal(debugView(expected), debugView(decompiled));
         }
     }
 }
