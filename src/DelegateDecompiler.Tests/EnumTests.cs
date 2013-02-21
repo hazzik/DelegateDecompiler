a082b6468b05e8abfffc25951dca128e46353d9d
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-22 10:09:43 +1300

    Add more tests for enums

--- src/DelegateDecompiler.Tests/EnumTests.cs
+++ src/DelegateDecompiler.Tests/EnumTests.cs
@@ -27,5 +27,37 @@ namespace DelegateDecompiler.Tests
             Func<TestEnum, bool> compiled = x => TestEnum.Bar == x;
             Test(expected, compiled);
         }
+
+        [Fact]
+        public void TestEnumParametersEqual()
+        {
+            Expression<Func<TestEnum, TestEnum, bool>> expected = (x, y) => x == y;
+            Func<TestEnum, TestEnum, bool> compiled = (x, y) => x == y;
+            Test(expected, compiled);
+        }
+
+        [Fact(Skip = "Needs optimization")]
+        public void TestEnumParameterNotEqualsEnumConstant()
+        {
+            Expression<Func<TestEnum, bool>> expected = x => x != TestEnum.Bar;
+            Func<TestEnum, bool> compiled = x => x != TestEnum.Bar;
+            Test(expected, compiled);
+        }
+
+        [Fact(Skip = "Needs optimization")]
+        public void TestEnumConstantNotEqualsEnumParameter()
+        {
+            Expression<Func<TestEnum, bool>> expected = x => TestEnum.Bar != x;
+            Func<TestEnum, bool> compiled = x => TestEnum.Bar != x;
+            Test(expected, compiled);
+        }
+
+        [Fact(Skip = "Needs optimization")]
+        public void TestEnumParametersNotEqual()
+        {
+            Expression<Func<TestEnum, TestEnum, bool>> expected = (x, y) => x != y;
+            Func<TestEnum, TestEnum, bool> compiled = (x, y) => x != y;
+            Test(expected, compiled);
+        }
     }
 }
