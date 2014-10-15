5b69d35193ad1493942dd6089de9524319596419
Alexander Zaytsev <hazzik@gmail.com>
2014-10-16 09:16:42 +1300

    Add build.bat, fix push from path with spaces

--- !push.cmd
+++ !push.cmd
@@ -1,1 +1,1 @@
-for /r %%x in (src\DelegateDecompiler\bin\Release\*.nupkg) do .nuget\NuGet.exe push %%x -ApiKey %1
+for /r %%x in (src\DelegateDecompiler\bin\Release\*.nupkg) do .nuget\NuGet.exe push "%%x" -ApiKey %1
---
+++ build.bat
@@ -0,0 +1,3 @@
+set EnableNuGetPackageRestore=true 
+set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild
+%msbuild% /p:Configuration=Release 
