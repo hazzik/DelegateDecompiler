6631f615e53338b2947279ca700ecc46d1bd6202
Giorgi Dalakishvili <giorgi@aboutmycode.com>
2014-10-16 15:11:26 +0400

    Added another Decompile extension method which can be used with methods that select single item (Any, Count, First, Single, etc)

--- src/DelegateDecompiler/DecompileExtensions.cs
+++ src/DelegateDecompiler/DecompileExtensions.cs
@@ -28,5 +28,12 @@ namespace DelegateDecompiler
             }
             return self;
         }
+
+        public static Expression<Func<T, bool>> Decompile<T>(Expression<Func<T, bool>> expression)
+        {
+            var decompiled = (Expression<Func<T, bool>>)DecompileExpressionVisitor.Decompile(expression);
+
+            return Expression.Lambda<Func<T, bool>>(decompiled.Body, expression.Parameters);
+        }
     }
 }
