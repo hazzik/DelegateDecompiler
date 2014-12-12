b01dbc3f09b5d727de46baa75406fdf1a81357e5
Alexander Zaytsev <hazzik@gmail.com>
2014-12-12 21:56:44 +1300

    Push EF package

--- !push.cmd
+++ !push.cmd
@@ -1,1 +1,3 @@
 for /r %%x in (src\DelegateDecompiler\bin\Release\*.nupkg) do .nuget\NuGet.exe push "%%x" -ApiKey %1
+for /r %%x in (src\DelegateDecompiler.EntityFramework\bin\Release\*.nupkg) do .nuget\NuGet.exe push "%%x" -ApiKey %1
+
