8c67bd96f54b0643324a4f192f7d2d5277275e4e
Alexander Zaytsev <hazzik@gmail.com>
2016-10-05 23:23:53 +1300

    Add tests for #85

---
+++ src/DelegateDecompiler.Tests/DateTimeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace DelegateDecompiler.Tests
+{
+    public class DbFunctions
+    {
+        public static DateTime? AddYears(DateTime? dateValue, int? addValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DateTimeTests : DecompilerTestsBase
+    {
+        [Test]
+        public void TestAddYears()
+        {
+            Func<DateTime, bool> actual = dt => dt < DbFunctions.AddYears(DateTime.Now, -10);
+            Expression< Func<DateTime, bool>> expected = dt => dt < DbFunctions.AddYears(DateTime.Now, -10);
+
+            Test(expected, actual);
+        }
+    }
+}
