264ddf37c230eb246ead3eeff428d614ff1d1b5c
Alexander Zaytsev <hazzik@gmail.com>
2016-12-25 13:19:32 +1300

    Code cleanup

--- src/DelegateDecompiler.EntityFramework/DecompileExtensions.cs
+++ src/DelegateDecompiler.EntityFramework/DecompileExtensions.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace DelegateDecompiler.EntityFramework
 {
     public static class DecompileExtensions
     {
-        private static readonly ConcurrentDictionary<MethodInfo, LambdaExpression> cache = new ConcurrentDictionary<MethodInfo, LambdaExpression>();
-
         public static IQueryable<T> DecompileAsync<T>(this IQueryable<T> self)
         {
-            var provider = new AsyncDecompiledQueryProvider(self.Provider);
-            return provider.CreateQuery<T>(self.Expression);
+            return new AsyncDecompiledQueryProvider(self.Provider).CreateQuery<T>(self.Expression);
         }
     }
 }
