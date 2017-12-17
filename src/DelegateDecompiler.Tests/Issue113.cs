c10a43315f84c0d0a68797948924a02dc3d80e5b
Max <max@micrologiciel.fr>
2017-12-17 10:43:41 +0100

    Workaround issue 113 : DecompileExpressionVisitor.VisitMethodCall with virtual computed methods

---
+++ src/DelegateDecompiler.Tests/Issue113.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq.Expressions;
+
+namespace DelegateDecompiler.Tests
+{
+    [TestFixture]
+    public class Issue113 : DecompilerTestsBase
+    {
+        private abstract class BaseClass
+        {
+            public int Property { get; set; }
+
+            [Computed]
+            public virtual int GetTotal()
+            {
+                return Property;
+            }
+        }
+
+        private class ConcreteClass
+            : BaseClass
+        {
+            public int OtherProperty { get; set; }
+
+            [Computed]
+            public override int GetTotal()
+            {
+                return base.GetTotal() + OtherProperty;
+            }
+        }
+
+        [Test]
+        public void DecompileOveriddenMemberWithBaseCall()
+        {
+            Expression<Func<ConcreteClass, int>> expected = x => x.Property + x.OtherProperty;
+            Expression<Func<ConcreteClass, int>> compiled = x => x.GetTotal();
+            var result = DecompileExpressionVisitor.Decompile(compiled);
+            Assert.AreEqual(expected.ToString(), result.ToString());
+            //Test(expected, compiled);
+        }
+    }
+}
--- src/DelegateDecompiler/DecompileExpressionVisitor.cs
+++ src/DelegateDecompiler/DecompileExpressionVisitor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -28,34 +27,86 @@ namespace DelegateDecompiler
             return base.VisitMember(node);
         }
 
+        private Dictionary<Tuple<Expression, string>, Type> _methodInheritanceContext = new Dictionary<Tuple<Expression, string>, Type>();
+
         protected override Expression VisitMethodCall(MethodCallExpression node)
         {
-            if (node.Method.IsGenericMethod && node.Method.GetGenericMethodDefinition() == typeof (ComputedExtension).GetMethod("Computed", BindingFlags.Static | BindingFlags.Public))
-            {
-                var argument = node.Arguments.SingleOrDefault();
+            // PROBLEM - the MethodCallExpression does not always (?never?) point to the most
+            // specific method for the object when a Computed method is overriden
 
-                var member = argument as MemberExpression;
-                if (member != null)
+            Expression instanceParameter = node.Object;
+            //Allows to decompile overriding implementations of a Computed method
+            bool shouldDecompileOverriddenCall = false;
+            try
+            {
+                // WORKAROUND - find the most specific implementation not used for the parameter and
+                // redirects the call the base implementation when the method is called again in the
+                // same decompilation unit
+
+                // Side Effect => this makes it impossible to use recursive calls but those would
+                // probably be refused by any IQueryProvider ?? Solution => add a configuration
+                // setting to allow this behaviour ?
+
+                // TODO Discover all specific implementations of the method sorted by specificity and
+                // expand the call into a : iif(o is XXX, o.SubType.Method, iif(..)) expression
+                if (node.Method.IsVirtual && instanceParameter != null && node.Object.Type != node.Method.DeclaringType)
                 {
-                    var info = member.Member as PropertyInfo;
-                    if (info != null)
+                    Tuple<Expression, string> implementation = new Tuple<Expression, string>(instanceParameter, node.Method.Name);
+                    Type currentType;
+                    if (!_methodInheritanceContext.TryGetValue(implementation, out currentType))
                     {
-                        return Decompile(info.GetGetMethod(), member.Expression, new List<Expression>());
+                        currentType = node.Object.Type;
+                        _methodInheritanceContext[implementation] = currentType;
+                    }
+                    else
+                    {
+                        currentType = currentType.BaseType;
+                    }
+                    MethodInfo method = currentType != null ? currentType.GetMethod(node.Method.Name, node.Arguments.Select(a => a.Type).ToArray()) : null;
+                    if (method != null && method != node.Method)
+                    {
+                        shouldDecompileOverriddenCall = true;
+                        currentType = method.DeclaringType;
+                        if (method.IsGenericMethod)
+                        {
+                            method = method.MakeGenericMethod(node.Method.GetGenericArguments());
+                        }
+                        node = Expression.Call(node.Object, method, node.Arguments);
                     }
                 }
-                var methodCall = argument as MethodCallExpression;
-                if (methodCall != null)
+                // END OF WORKAROUND
+                if (node.Method.IsGenericMethod && node.Method.GetGenericMethodDefinition() == typeof(ComputedExtension).GetMethod("Computed", BindingFlags.Static | BindingFlags.Public))
                 {
-                    return Decompile(methodCall.Method, methodCall.Object, methodCall.Arguments);
+                    var argument = node.Arguments.SingleOrDefault();
+
+                    var member = argument as MemberExpression;
+                    if (member != null)
+                    {
+                        var info = member.Member as PropertyInfo;
+                        if (info != null)
+                        {
+                            return Decompile(info.GetGetMethod(), member.Expression, new List<Expression>());
+                        }
+                    }
+                    var methodCall = argument as MethodCallExpression;
+                    if (methodCall != null)
+                    {
+                        return Decompile(methodCall.Method, instanceParameter, methodCall.Arguments);
+                    }
                 }
-            }
 
-            if (ShouldDecompile(node.Method))
+                if (shouldDecompileOverriddenCall || ShouldDecompile(node.Method))
+                {
+                    return Decompile(node.Method, node.Object, node.Arguments);
+                }
+
+                return base.VisitMethodCall(node);
+            }
+            finally
             {
-                return Decompile(node.Method, node.Object, node.Arguments);
+                foreach (var parameterKey in _methodInheritanceContext.Keys.Where(key => key.Item1 == instanceParameter).ToList())
+                    _methodInheritanceContext.Remove(parameterKey);
             }
-
-            return base.VisitMethodCall(node);
         }
 
         protected virtual bool ShouldDecompile(MemberInfo methodInfo)
@@ -63,7 +114,7 @@ namespace DelegateDecompiler
             return Configuration.Instance.ShouldDecompile(methodInfo);
         }
 
-        Expression Decompile(MethodInfo method, Expression instance, IList<Expression> arguments)
+        private Expression Decompile(MethodInfo method, Expression instance, IList<Expression> arguments)
         {
             var expression = method.Decompile();
 
@@ -81,8 +132,8 @@ namespace DelegateDecompiler
                     expressions.Add(parameter, arguments[argIndex++]);
                 }
             }
-
-            return Visit(new ReplaceExpressionVisitor(expressions).Visit(expression.Body));
+            Expression result = new ReplaceExpressionVisitor(expressions).Visit(expression.Body);
+            return Visit(result);
         }
     }
 }
