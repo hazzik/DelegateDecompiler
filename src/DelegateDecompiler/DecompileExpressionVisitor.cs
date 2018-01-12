6729271dc424c5abe6c958b55ff85b86c88793a9
Max <max@micrologiciel.fr>
2018-01-12 17:36:25 +0100

    Fixing wrong placement for test

--- src/DelegateDecompiler/DecompileExpressionVisitor.cs
+++ src/DelegateDecompiler/DecompileExpressionVisitor.cs
@@ -52,7 +52,6 @@ namespace DelegateDecompiler
                     {
                         Expression expandedCall = null;
                         mostSpecificImplementation = (implementations as List<KeyValuePair<Type, MethodInfo>>).Where(vCall => vCall.Key.IsAssignableFrom(node.Object.Type)).Select(kv => kv.Value).LastOrDefault();
-                        if (mostSpecificImplementation == null) return node;
 
                         var overridingImplementations = (implementations as List<KeyValuePair<Type, MethodInfo>>).Where(vCall => vCall.Key.IsSubclassOf(node.Object.Type)).GroupBy(kv => kv.Key);
                         if (overridingImplementations.Any())
@@ -88,6 +87,7 @@ namespace DelegateDecompiler
                     {
                         mostSpecificImplementation = implementations as MethodInfo;
                     }
+                    if (mostSpecificImplementation == null) return node;
                     node = Expression.Call(node.Object, mostSpecificImplementation, node.Arguments);
                 }
                 finally
