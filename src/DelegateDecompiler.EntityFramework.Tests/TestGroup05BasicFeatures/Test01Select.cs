84893850ea0e75181f03dcfac3d20282cbab7cc5
Max <max@micrologiciel.fr>
2018-04-29 10:32:28 +0200

    Refactored test

--- src/DelegateDecompiler.EntityFramework.Tests/TestGroup05BasicFeatures/Test01Select.cs
+++ src/DelegateDecompiler.EntityFramework.Tests/TestGroup05BasicFeatures/Test01Select.cs
@@ -179,7 +179,7 @@ namespace DelegateDecompiler.EntityFramework.Tests.TestGroup05BasicFeatures
             using (var env = new MethodEnvironment(classEnv))
             {
                 env.AboutToUseDelegateDecompiler();
-                var persons = env.Db.Set<Person>();
+                var persons = env.Db.Set<Person>(); //env.Db.LivingBeeing.OfType<Person>();
                 var expr = env.Db.Set<Cat>().Where(c => c.IsAdopted(persons)).Decompile();
                 var result = expr.ToList();
             }
