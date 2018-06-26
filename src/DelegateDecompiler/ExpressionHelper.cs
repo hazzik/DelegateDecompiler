2c30887c0851571836409c083d7b1e4ff81c342e
Alexander Zaytsev <hazzik@gmail.com>
2018-06-27 10:57:41 +1200

    Remove unused method

--- src/DelegateDecompiler/ExpressionHelper.cs
+++ src/DelegateDecompiler/ExpressionHelper.cs
@@ -9,20 +9,5 @@ namespace DelegateDecompiler
             // LINQ to entities and possibly other providers don't support Expression.Default, so
             // this gets the default value and then uses an Expression.Constant instead
             Expression.Constant(type.IsValueType ? Activator.CreateInstance(type) : null, type);
-
-        internal static Expression DiscardConversion(this Expression expr)
-        {
-            if (!(expr is UnaryExpression)) return expr;
-            while (expr is UnaryExpression && (expr.NodeType == ExpressionType.Convert || expr.NodeType == ExpressionType.ConvertChecked))
-            {
-                expr = (expr as UnaryExpression).Operand;
-            }
-            return expr;
-        }
-
-        internal static Address DiscardConversion(this Address expr)
-        {
-            return DiscardConversion((Expression)expr);
-        }
     }
 }
