73c475894ae39fc16d4ef8a8e42e56096aadfa23
Alex Zaytsev <hazzik@gmail.com>
2021-12-14 22:02:09 +1300

    Remove .NET Core 2.1 target from test projects

--- test.cmd
+++ test.cmd
@@ -6,10 +6,9 @@ nunit3-console ^
  src\DelegateDecompiler.EntityFramework.Tests\bin\Release\net45\DelegateDecompiler.EntityFramework.Tests.dll ^
  --result=DelegateDecompiler.testsresults.xml;format=AppVeyor
 
-dotnet test --no-build -c Release -f netcoreapp2.1 src\DelegateDecompiler.Tests 
-dotnet test --no-build -c Release -f netcoreapp2.1 src\DelegateDecompiler.Tests.VB
+dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.Tests 
+dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.Tests.VB
 dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.EntityFramework.Tests
-dotnet test --no-build -c Release -f netcoreapp2.1 src\DelegateDecompiler.EntityFrameworkCore.Tests
 dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.EntityFrameworkCore.Tests
 dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.EntityFrameworkCore3.Tests
 dotnet test --no-build -c Release -f net5.0 src\DelegateDecompiler.EntityFrameworkCore5.Tests
