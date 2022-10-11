45743565b75e6ea318a2beb1bf8eb63681737b94
Alex Zaytsev <hazzik@gmail.com>
2022-10-12 12:11:23 +1300

    fixup! Add support for simple nested expressions

--- src/DelegateDecompiler/OptimizeExpressionVisitor.cs
+++ src/DelegateDecompiler/OptimizeExpressionVisitor.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Security.AccessControl;
 
 namespace DelegateDecompiler
 {
