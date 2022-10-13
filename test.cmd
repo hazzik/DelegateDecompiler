2e0e3612f40c9d162f17b93a495d3b100ef5fdf4
Alex Zaytsev <hazzik@gmail.com>
2022-10-13 13:43:09 +1300

    exit error level

--- test.cmd
+++ test.cmd
@@ -11,3 +11,5 @@ dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.Entity
 dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.EntityFrameworkCore.Tests & ^
 dotnet test --no-build -c Release -f netcoreapp3.1 src\DelegateDecompiler.EntityFrameworkCore3.Tests & ^
 dotnet test --no-build -c Release -f net5.0 src\DelegateDecompiler.EntityFrameworkCore5.Tests
+
+exit /b %ERRORLEVEL%
