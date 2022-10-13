0b1ef1f8a08099e7c1571cb118bf9167fc68e8dd
Alex Zaytsev <hazzik@gmail.com>
2022-10-13 17:44:18 +1300

    Verify that #90 works

---
+++ src/DelegateDecompiler.Tests/Issue90.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DelegateDecompiler.Tests
+{
+    public class Issue90
+    {
+        public class Test 
+        { 
+            public string A { get; set; }
+        }
+
+        public class Test2
+        {
+            public List<Test> Collection { get; } = new List<Test>();
+
+            [Computed]
+            public Test Last => Collection.LastOrDefault();
+        }
+
+        [Test]
+        public void ShouldBeAbleToDecompileLastOrDefault()
+        {
+            var actual = Enumerable.Range(0, 3)
+                .Select(x => new Test2
+                {
+                    Collection =
+                    {
+                        new Test { A = (x * 3 + 0).ToString() },
+                        new Test { A = (x * 3 + 1).ToString() },
+                        new Test { A = (x * 3 + 2).ToString() },
+                    }
+                })
+                .AsQueryable()
+                .Select(x => x.Last)
+                .Select(x => x.A)
+                .Decompile()
+                .ToList();
+
+            Assert.That(actual, Is.EqualTo(new[] { "2", "5", "8" }));
+        }
+    }
+}
