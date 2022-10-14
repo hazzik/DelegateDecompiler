22a0ebbcb5a36011df71c31a818f6255790c74c2
Carl Meyertons <carl.meyertons@pwc.com>
2022-10-14 16:35:22 -0500

    interfaces

--- src/DelegateDecompiler/DecompileExpressionVisitor.cs
+++ src/DelegateDecompiler/DecompileExpressionVisitor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -7,9 +8,16 @@ namespace DelegateDecompiler
 {
     public class DecompileExpressionVisitor : ExpressionVisitor
     {
-        public static Expression Decompile(Expression expression)
+        private readonly Type _declaringType;
+
+        public static Expression Decompile(Expression expression, Type declaringType = null)
         {
-            return new DecompileExpressionVisitor().Visit(expression);
+            return new DecompileExpressionVisitor(declaringType).Visit(expression);
+        }
+
+        public DecompileExpressionVisitor(Type declaringType)
+        {
+            _declaringType = declaringType;
         }
 
         protected override Expression VisitMember(MemberExpression node)
@@ -24,20 +32,20 @@ namespace DelegateDecompiler
 
         protected override Expression VisitMethodCall(MethodCallExpression node)
         {
-            if (node.Method.IsGenericMethod && node.Method.GetGenericMethodDefinition() == typeof (ComputedExtension).GetMethod("Computed", BindingFlags.Static | BindingFlags.Public))
+            if (node.Method.IsGenericMethod && node.Method.GetGenericMethodDefinition() == typeof(ComputedExtension).GetMethod("Computed", BindingFlags.Static | BindingFlags.Public))
             {
                 var argument = node.Arguments.SingleOrDefault();
 
                 switch (argument)
                 {
                     case MemberExpression member when member.Member is PropertyInfo property:
-                    {
-                        return Decompile(property.GetGetMethod(true), member.Expression, new List<Expression>());
-                    }
+                        {
+                            return Decompile(property.GetGetMethod(true), member.Expression, new List<Expression>());
+                        }
                     case MethodCallExpression methodCall:
-                    {
-                        return Decompile(methodCall.Method, methodCall.Object, methodCall.Arguments);
-                    }
+                        {
+                            return Decompile(methodCall.Method, methodCall.Object, methodCall.Arguments);
+                        }
                 }
             }
 
@@ -56,7 +64,13 @@ namespace DelegateDecompiler
 
         Expression Decompile(MethodInfo method, Expression instance, IList<Expression> arguments)
         {
-            var expression = method.Decompile(instance?.Type);
+            // for an expression that flowed through a generic, we want to check if we did a conversion to a virtual type from our initial type
+            var instanceParameter = instance is UnaryExpression unary && unary.NodeType == ExpressionType.Convert && unary.Operand is ParameterExpression ip && ip.Type == _declaringType
+                ? ip
+                : null
+            ;
+
+            var expression = method.Decompile(instanceParameter?.Type ?? instance?.Type);
 
             var expressions = new Dictionary<Expression, Expression>();
             var argIndex = 0;
@@ -65,7 +79,7 @@ namespace DelegateDecompiler
                 var parameter = expression.Parameters[index];
                 if (index == 0 && method.IsStatic == false)
                 {
-                    expressions.Add(parameter, instance);
+                    expressions.Add(parameter, instanceParameter as Expression ?? instance);
                 }
                 else
                 {
