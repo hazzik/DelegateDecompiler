c7967f16b94e21942093424613c2c74c0d317e56
magicmoux <magicmoux@gmail.com>
2022-10-17 03:23:37 +0200

    Refactoring local methods as Expression extentions (#197)
    +semver:patch

---
+++ src/DelegateDecompiler/ExpressionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DelegateDecompiler
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression Decompile(this Expression expression)
+        {
+            return DecompileExpressionVisitor.Decompile(expression);
+        }
+
+        public static Expression Optimize(this Expression expression)
+        {
+            return OptimizeExpressionVisitor.Optimize(expression);
+        }
+
+        public static T Evaluate<T>(this Expression expression)
+        {
+            var func = Expression.Lambda<Func<T>>(expression).Compile();
+            return func.Invoke();
+        }
+    }
+}
