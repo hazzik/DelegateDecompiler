a065715e668a4d4afec6f637a6f1f6192cda713c
Alex Zaytsev <hazzik@gmail.com>
2025-03-01 12:28:26 +1000

    Fix test.cmd exit code

--- test.cmd
+++ test.cmd
@@ -5,9 +5,9 @@ nunit3-console ^
  src\DelegateDecompiler.Tests.VB\bin\Release\net45\DelegateDecompiler.Tests.VB.dll ^
  src\DelegateDecompiler.EntityFramework.Tests\bin\Release\net45\DelegateDecompiler.EntityFramework.Tests.dll ^
  --result=DelegateDecompiler.testsresults.xml;format=AppVeyor && ^
-
 dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.Tests && ^
 dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.Tests.VB && ^
 dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.EntityFramework.Tests && ^
-dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.EntityFrameworkCore6.Tests
-dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.EntityFrameworkCore8.Tests
+dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.EntityFrameworkCore6.Tests && ^
+dotnet test --no-build -c Release -f net8.0 src\DelegateDecompiler.EntityFrameworkCore8.Tests && ^
+exit /b
