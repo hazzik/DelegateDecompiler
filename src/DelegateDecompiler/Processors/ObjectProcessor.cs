98555652b9b958f7ab6175e677f5d019f640c461
Alex Zaytsev <hazzik@gmail.com>
2025-10-02 16:24:03 +1000

    Fix compilation

--- src/DelegateDecompiler/Processors/ObjectProcessor.cs
+++ src/DelegateDecompiler/Processors/ObjectProcessor.cs
@@ -8,26 +8,31 @@ internal class ObjectProcessor : IProcessor
 {
     public bool Process(ProcessorState state)
     {
-        switch (state.Instruction.OpCode)
+        if (state.Instruction.OpCode == OpCodes.Initobj)
         {
-            case OpCodes.Initobj:
-                var address = state.Stack.Pop();
-                var type = (Type)state.Instruction.Operand;
-                address.Expression = ExpressionHelper.Default(type);
-                return true;
-            case OpCodes.Newarr:
-                var operand = (Type)state.Instruction.Operand;
-                var expression = state.Stack.Pop();
-                if (expression.Expression is ConstantExpression size && (int)size.Value == 0) // optimization
-                    state.Stack.Push(Expression.NewArrayInit(operand));
-                else
-                    state.Stack.Push(Expression.NewArrayBounds(operand, expression));
-                return true;
-            case OpCodes.Box:
-                state.Stack.Push(Processor.Box(state.Stack.Pop(), (Type)state.Instruction.Operand));
-                return true;
-            default:
-                return false;
+            var address = state.Stack.Pop();
+            var type = (Type)state.Instruction.Operand;
+            address.Expression = ExpressionHelper.Default(type);
+            return true;
         }
+
+        if (state.Instruction.OpCode == OpCodes.Newarr)
+        {
+            var operand = (Type)state.Instruction.Operand;
+            var expression = state.Stack.Pop();
+            if (expression.Expression is ConstantExpression size && (int)size.Value == 0) // optimization
+                state.Stack.Push(Expression.NewArrayInit(operand));
+            else
+                state.Stack.Push(Expression.NewArrayBounds(operand, expression));
+            return true;
+        }
+
+        if (state.Instruction.OpCode == OpCodes.Box)
+        {
+            state.Stack.Push(Processor.Box(state.Stack.Pop(), (Type)state.Instruction.Operand));
+            return true;
+        }
+
+        return false;
     }
 }
