005e4a2130f0aa794776694202ff3cb2f592c419
Alex Zaytsev <hazzik@gmail.com>
2025-10-02 17:43:19 +1000

    Fix AI slop

--- src/DelegateDecompiler/Processor.cs
+++ src/DelegateDecompiler/Processor.cs
@@ -499,56 +499,7 @@ namespace DelegateDecompiler
             return expression;
         }
 
-        static void StElem(ProcessorState state)
-        {
-            var value = state.Stack.Pop();
-            var index = state.Stack.Pop();
-            var array = state.Stack.Pop();
-
-            if (array.Expression is NewArrayExpression newArray)
-            {
-                var elementType = array.Type.GetElementType();
-                var expressions = CreateArrayInitExpressions(elementType, newArray, value, index);
-                array.Expression = Expression.NewArrayInit(elementType, expressions);
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        static IEnumerable<Expression> CreateArrayInitExpressions(
-            Type elementType, NewArrayExpression newArray, Expression valueExpression, Expression indexExpression)
-        {
-            var indexGetter = (Func<int>)Expression.Lambda(indexExpression).Compile();
-            var index = indexGetter();
-
-            Expression[] expressions;
-            if (newArray.NodeType == ExpressionType.NewArrayInit)
-            {
-                expressions = newArray.Expressions.ToArray();
-                if (index >= newArray.Expressions.Count)
-                {
-                    Array.Resize(ref expressions, index + 1);
-                }
-
-            }
-            else if (newArray.NodeType == ExpressionType.NewArrayBounds)
-            {
-                var sizeExpression = newArray.Expressions.Single();
-                var sizeGetter = (Func<int>)Expression.Lambda(sizeExpression).Compile();
-                var getter = sizeGetter();
-
-                expressions = Enumerable.Repeat(ExpressionHelper.Default(elementType), getter).ToArray();
-            }
-            else
-            {
-                throw new NotSupportedException();
-            }
-
-            expressions[index] = AdjustType(valueExpression, elementType);
-            return expressions;
-        }
+       
 
         static void Call(ProcessorState state, MethodInfo m)
         {
--- src/DelegateDecompiler/Processors/StelemProcessor.cs
+++ src/DelegateDecompiler/Processors/StelemProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection.Emit;
 
@@ -7,44 +8,76 @@ namespace DelegateDecompiler.Processors;
 
 internal class StelemProcessor : IProcessor
 {
-    static readonly Dictionary<OpCode, Type> StelemTypes = new()
+    static readonly HashSet<OpCode> Operations = new()
     {
-        { OpCodes.Stelem_I, typeof(IntPtr) },
-        { OpCodes.Stelem_I1, typeof(sbyte) },
-        { OpCodes.Stelem_I2, typeof(short) },
-        { OpCodes.Stelem_I4, typeof(int) },
-        { OpCodes.Stelem_I8, typeof(long) },
-        { OpCodes.Stelem_R4, typeof(float) },
-        { OpCodes.Stelem_R8, typeof(double) },
-        { OpCodes.Stelem_Ref, typeof(object) }
+        OpCodes.Stelem,
+        OpCodes.Stelem_I,
+        OpCodes.Stelem_I1,
+        OpCodes.Stelem_I2,
+        OpCodes.Stelem_I4,
+        OpCodes.Stelem_I8,
+        OpCodes.Stelem_R4,
+        OpCodes.Stelem_R8,
+        OpCodes.Stelem_Ref
     };
 
     public bool Process(ProcessorState state)
     {
-        if (state.Instruction.OpCode == OpCodes.Stelem)
-        {
-            var type = (Type)state.Instruction.Operand;
-            StElem(state, type);
-            return true;
-        }
+        if (!Operations.Contains(state.Instruction.OpCode))
+            return false;
 
-        if (StelemTypes.TryGetValue(state.Instruction.OpCode, out var stelemType))
-        {
-            StElem(state, stelemType);
-            return true;
-        }
-
-        return false;
+        StElem(state);
+        return true;
     }
 
-    static void StElem(ProcessorState state, Type type)
+    static void StElem(ProcessorState state)
     {
         var value = state.Stack.Pop();
         var index = state.Stack.Pop();
         var array = state.Stack.Pop();
 
-        var arrayAccess = Expression.ArrayAccess(array, index);
-        var assignment = Expression.Assign(arrayAccess, Expression.Convert(value, type));
-        state.Stack.Push(assignment);
+        if (array.Expression is NewArrayExpression newArray)
+        {
+            var elementType = array.Type.GetElementType();
+            var expressions = CreateArrayInitExpressions(elementType, newArray, value, index);
+            array.Expression = Expression.NewArrayInit(elementType, expressions);
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    static IEnumerable<Expression> CreateArrayInitExpressions(
+        Type elementType, NewArrayExpression newArray, Expression valueExpression, Expression indexExpression)
+    {
+        var indexGetter = (Func<int>)Expression.Lambda(indexExpression).Compile();
+        var index = indexGetter();
+
+        Expression[] expressions;
+        if (newArray.NodeType == ExpressionType.NewArrayInit)
+        {
+            expressions = newArray.Expressions.ToArray();
+            if (index >= newArray.Expressions.Count)
+            {
+                Array.Resize(ref expressions, index + 1);
+            }
+
+        }
+        else if (newArray.NodeType == ExpressionType.NewArrayBounds)
+        {
+            var sizeExpression = newArray.Expressions.Single();
+            var sizeGetter = (Func<int>)Expression.Lambda(sizeExpression).Compile();
+            var getter = sizeGetter();
+
+            expressions = Enumerable.Repeat(ExpressionHelper.Default(elementType), getter).ToArray();
+        }
+        else
+        {
+            throw new NotSupportedException();
+        }
+
+        expressions[index] = Processor.AdjustType(valueExpression, elementType);
+        return expressions;
     }
 }
