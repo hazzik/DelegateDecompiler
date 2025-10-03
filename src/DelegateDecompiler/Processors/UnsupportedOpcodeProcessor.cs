8b02c52730562fc0d6b594bfbb2bd1f85f942d45
Alex Zaytsev <hazzik@gmail.com>
2025-10-03 20:22:16 +1000

    Cleanup

--- src/DelegateDecompiler/Processors/UnsupportedOpcodeProcessor.cs
+++ src/DelegateDecompiler/Processors/UnsupportedOpcodeProcessor.cs
@@ -5,20 +5,9 @@ namespace DelegateDecompiler.Processors;
 
 internal class UnsupportedOpcodeProcessor : IProcessor
 {
-    public bool Process(ProcessorState state)
-    {
-        // This processor handles all opcodes that are not supported by other processors
-        // by throwing a descriptive NotSupportedException
-        if (state?.Instruction == null)
-        {
-            throw new NotSupportedException("Cannot process null instruction. " +
-                                          "This indicates an internal error in the decompilation process.");
-        }
-        
-        var opCode = state.Instruction.OpCode;
-        
-        throw new NotSupportedException($"The IL opcode '{opCode}' is not supported by DelegateDecompiler. " +
-                                      $"This opcode cannot be decompiled into an expression tree. " +
-                                      $"Consider simplifying the method or using a different approach.");
-    }
+    public bool Process(ProcessorState state) =>
+        throw new NotSupportedException(
+            $"The IL opcode '{state.Instruction.OpCode}' is not supported by DelegateDecompiler. " +
+            $"This opcode cannot be decompiled into an expression tree. " +
+            $"Consider simplifying the method or using a different approach.");
 }
